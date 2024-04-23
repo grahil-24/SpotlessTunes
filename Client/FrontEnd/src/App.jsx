5e926bef04bf07e9dcb667a6683961ba31c900d8
rahil <rahilganatra@gmail.com>
2024-04-23 17:57:09 +0530

    protected routes

--- Client/FrontEnd/src/App.jsx
+++ Client/FrontEnd/src/App.jsx
@@ -1,46 +1,48 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import Login from "./components/Login.jsx";
 import RemoveDuplicates from "./components/RemoveDuplicates.jsx";
 import AuthCodeHandler from "./components/AuthCodeHandler.jsx";
 import NavBar from "./components/Navbar.jsx";
 import Home from "./components/Home.jsx";
-import { AccessTokenProvider } from "./components/AccessTokenContext.jsx";
 import Statistics from "./components/Statistics.jsx";
 import AboutPage from "./components/AboutPage.jsx";
 import Contact from "./components/Contact.jsx";
 import SearchSongs from "./components/SearchSongs.jsx";
 import SongMeaning from "./components/SongMeaning.jsx";
-import { ChakraProvider } from "@chakra-ui/react";
 import Review from "./components/Review.jsx";
 function App() {
   // const code = new URLSearchParams(window.location.search).get("code");
   // const accessToken = useAuth(code);
 
+    const ProtectedRoute = ({ element: Element, ...rest }) => {
+        // Check if accessToken is present in localStorage
+        const accessToken = localStorage.getItem('accessToken');
+
+        // If accessToken is not present, redirect to login page
+        if (!accessToken) {
+            return <Navigate to="/" replace />;
+        }
+
+        // Render the route with the provided element
+        return <Route {...rest} element={<Element />} />;
+    };
+
   return (
       <>
         <Router>
-          <AccessTokenProvider>
             <NavBar />
             <Routes>
               <Route path="/" element={<Login />} />
-              <Route
-                  path="/home"
-                  element={
-                    <ChakraProvider>
-                      <Home />
-                    </ChakraProvider>
-                  }
-              />
-              <Route path="/remove-duplicates" element={<RemoveDuplicates />} />
+                <ProtectedRoute path="/home" element={<Home />} />
+                <ProtectedRoute path="/remove-duplicates" element={<RemoveDuplicates />} />
               <Route path="/auth" element={<AuthCodeHandler />} />
-              <Route path="/stats" element={<Statistics />} />
+                <ProtectedRoute path="/stats" element={<Statistics />} />
               <Route path="/about" element={<AboutPage />} />
               <Route path="/contact" element={<Contact />} />
-              <Route path="/search-songs" element={<SearchSongs />} />
-              <Route path="/get-meaning" element={<SongMeaning />} />
-              <Route path="/review" element={<Review />} />
+                <ProtectedRoute path="/search-songs" element={<SearchSongs />} />
+                <ProtectedRoute path="/get-meaning" element={<SongMeaning />} />
+                <ProtectedRoute path="/review" element={<Review />} />
             </Routes>
-          </AccessTokenProvider>
         </Router>
       </>
   );

e84b52b342c788491489a14625c280b88d9d829f
grahil-24 <94360103+grahil-24@users.noreply.github.com>
2024-04-23 18:12:34 +0530

    roll back

--- Client/FrontEnd/src/App.jsx
+++ Client/FrontEnd/src/App.jsx
@@ -33,15 +33,22 @@ function App() {
              <NavBar />
             <Routes>
               <Route path="/" element={<Login />} />
-                <ProtectedRoute path="/home" element={<Home />} />
-                <ProtectedRoute path="/remove-duplicates" element={<RemoveDuplicates />} />
+              <Route
+                  path="/home"
+                  element={
+                    <ChakraProvider>
+                      <Home />
+                    </ChakraProvider>
+                  }
+              />
+              <Route path="/remove-duplicates" element={<RemoveDuplicates />} />
               <Route path="/auth" element={<AuthCodeHandler />} />
-                <ProtectedRoute path="/stats" element={<Statistics />} />
+              <Route path="/stats" element={<Statistics />} />
               <Route path="/about" element={<AboutPage />} />
               <Route path="/contact" element={<Contact />} />
-                <ProtectedRoute path="/search-songs" element={<SearchSongs />} />
-                <ProtectedRoute path="/get-meaning" element={<SongMeaning />} />
-                <ProtectedRoute path="/review" element={<Review />} />
+              <Route path="/search-songs" element={<SearchSongs />} />
+              <Route path="/get-meaning" element={<SongMeaning />} />
+              <Route path="/review" element={<Review />} />
             </Routes>
         </Router>
       </>
